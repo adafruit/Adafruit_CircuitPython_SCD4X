7d6aaff18b716438fb7115b2d7d29d787947901e
CrakeNotSnowman <kmurrayis@gmail.com>
2021-08-27 14:19:54 -0500

    Fixed spelling and adjusted example design to more closely match the scd4x datasheet

--- examples/scd41_single_shot_example.py
+++ examples/scd41_single_shot_example.py
@@ -9,8 +9,8 @@ i2c = board.I2C()
 scd4x = adafruit_scd4x.SCD4X(i2c)
 print("Serial number:", [hex(i) for i in scd4x.serial_number])
 
+print("Waiting for single shot CO2 measurement from SCD41....")
 scd4x.measure_single_shot()
-print("Waiting for single show CO2 measurement from SCD41....")
 
 sample_counter = 0
 while sample_counter < 3:
@@ -19,22 +19,24 @@ while sample_counter < 3:
         print("Temperature: %0.1f *C" % scd4x.temperature)
         print("Humidity: %0.1f %%" % scd4x.relative_humidity)
         print()
+        scd4x.measure_single_shot()
         sample_counter += 1
     else:
         print("Waiting...")
     time.sleep(1)
 
 
+print("Waiting for single shot Humidity and Temperature measurement from SCD41....")
 scd4x.measure_single_shot_rht_only()
-print("Waiting for single show Humidity and Temperature measurement from SCD41....")
 
 sample_counter = 0
 while sample_counter < 3:
     if scd4x.data_ready:
-        print("CO2: %d ppm" % scd4x.CO2) # Should be 0 ppm
+        print("CO2: %d ppm" % scd4x.CO2)  # Should be 0 ppm
         print("Temperature: %0.1f *C" % scd4x.temperature)
         print("Humidity: %0.1f %%" % scd4x.relative_humidity)
         print()
+        scd4x.measure_single_shot_rht_only()
         sample_counter += 1
     else:
         print("Waiting...")
